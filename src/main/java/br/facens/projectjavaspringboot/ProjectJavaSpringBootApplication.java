2a971e56743c4c6c5da94c25f1f6f53d144c30eb
Gabriel Nogueira <gabrielnoog7@gmail.com>
2021-03-15 23:39:13 -0300

    Create a project Java SpringBoot

---
+++ src/main/java/br/facens/projectjavaspringboot/ProjectJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.facens.projectjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJavaSpringBootApplication.class, args);
+	}
+
+}
