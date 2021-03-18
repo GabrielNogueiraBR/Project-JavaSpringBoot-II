a8aa0d378cfc5770587dc554f2704e3fd15d34ac
Gabriel Nogueira <gabrielnoog7@gmail.com>
2021-03-17 23:48:18 -0300

    Update files properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_event (name,description,place,start_date,end_date,start_time,end_time,contact) VALUES ('Deus Chamma','Grupo de Oração Jovem','Igreja São José','20-03-2021','20-03-2021','19:00','21:30','deuschamma06@gmail.com')
