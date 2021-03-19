53c35bd20d1ed434203dd5f55c2aa331586781e5
Gabriel Nogueira <gabrielnoog7@gmail.com>
2021-03-18 21:53:56 -0300

    Fix error in eventController in endpoint POST

--- src/main/java/br/facens/projectjavaspringboot/controllers/EventController.java
+++ src/main/java/br/facens/projectjavaspringboot/controllers/EventController.java
@@ -3,13 +3,13 @@ package br.facens.projectjavaspringboot.controllers;
 import java.net.URI;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +39,7 @@ public class EventController {
         return ResponseEntity.ok(eDto);
     }
 
-    @PostConstruct
+    @PostMapping
     public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventInsertDTO insertDto){
         EventDTO eventDTO = service.insert(insertDto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(eventDTO.getId()).toUri();
