d5904367ae8dfe4aef5d0783e081f783bad93646
Gabriel Nogueira <gabrielnoog7@gmail.com>
2021-04-20 23:04:05 -0300

    Add a register in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO tb_event (name,description,place,start_date,end_date,start_time,end_
 
 INSERT INTO tb_event (name,description,place,start_date,end_date,start_time,end_time,email_contact) VALUES ('Festa Junina Votorantim','Festa que acontece no mês de Junho','Plaça de Eventos','2021-05-01','2021-05-30','19:00','23:30','festajuninavotorantim@gmail.com')
 
+INSERT INTO tb_event (name,description,place,start_date,end_date,start_time,end_time,email_contact) VALUES ('Teatro Paixão de Cristo','Teatro da paixão de cristo da Paróquia São José.','Igreja São José','2021-04-02','2021-04-02','13:00','16:30','paroquiasaojosevotorantim@gmail.com')
